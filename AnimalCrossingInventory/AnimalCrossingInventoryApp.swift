4b7e7773fd3b9fdd82b035422f04c88b077103df
Sarah Watremet <sarah.watremet@gmail.com>
2022-06-03 14:29:52 +0200

    Initial Commit

---
+++ AnimalCrossingInventory/AnimalCrossingInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCrossingInventoryApp.swift
+//  AnimalCrossingInventory
+//
+//  Created by Sarah Watremet on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCrossingInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalCrossingInventory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalCrossingInventory
+//
+//  Created by Sarah Watremet on 03/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
